3d06b50f338bf083a1f1b8a9e09bbf3941b1af92
lukegro <lukasz.grodek@gmail.com>
2017-12-09 08:25:53 +0100

    second commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/views/app.component.html'
+})
+export class AppComponent  {
+  name: string = 'World';
+  appStatus: boolean = true;
+  appList: any[] = [{
+    "ID": "1",
+    "Name" : "One"
+  },
+  {
+    "ID": "2",
+    "Name" : "Two"
+  }
+  ];
+}
